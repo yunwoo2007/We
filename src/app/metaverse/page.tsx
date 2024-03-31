c448dd58dc3edf0e49f037c7e81fb8302622e7bb
yunwoo2007 <154660238+yunwoo2007@users.noreply.github.com>
2024-03-31 14:24:48 +0900

    Add files via upload

---
+++ src/app/metaverse/page.tsx
@@ -0,0 +1,9 @@
+import { Layout } from '../layout';
+
+const Page = () => (
+    <Layout title="메타버스">
+        <div>이 페이지는 메타버스 페이지입니다.</div>
+    </Layout>
+);
+
+export default Page;
---
+++ src/app/town/page.tsx
@@ -0,0 +1,9 @@
+import { Layout } from '../layout';
+
+const Page = () => (
+    <Layout title="마을의 역사">
+        <div>이 페이지는 마을의 역사 페이지입니다.</div>
+    </Layout>
+);
+
+export default Page;
